c6281b060cc5893ad6c21543d52ad6dca62e1ceb
Sonik4gain <sonik.gain25@gmail.com>
2026-02-05 14:52:36 +0300

    Initial commit

---
+++ Backend/src/main/java/com/project/_5/Friend_Finder/FriendFinderApplication.java
@@ -0,0 +1,13 @@
+package com.project._5.Friend_Finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendFinderApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/project/_5/Friend_Finder/FriendFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project._5.Friend_Finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
